3856d61b98d597317eaa11927183e0df1b7349c1
Rory malone <rory.malone@hotmail.com>
2020-09-05 20:50:16 +0100

    fix: dont generate new tile if cant move

--- src/App.tsx
+++ src/App.tsx
@@ -13,55 +13,53 @@ function App() {
     return [tile1, tile2]
   };
 
-
   const [gridSize, setGridSize] = useState(4);
-  // const [tiles, setTiles] = useState<ITile[]>(startGame());
-  const [tiles, setTiles] = useState<ITile[]>([
-    {
-      id: '1',
-      value: 2,
-      coord: {
-        y: 0,
-        x: 0
-      }
-    },
-    {
-      id: '2',
-      value: 16,
-      coord: {
-        y: 0,
-        x: 1
-      }
-    },
-    {
-      id: '3',
-      value: 16,
-      coord: {
-        y: 0,
-        x: 2
-      }
-    },
-    {
-      id: '4',
-      value: 16,
-      coord: {
-        y: 0,
-        x: 3
-      }
-    }
-  ]);
+  const [tiles, setTiles] = useState<ITile[]>(startGame());
+  // const [tiles, setTiles] = useState<ITile[]>([
+  //   {
+  //     id: '1',
+  //     value: 2,
+  //     coord: {
+  //       y: 0,
+  //       x: 0
+  //     }
+  //   },
+  //   {
+  //     id: '2',
+  //     value: 16,
+  //     coord: {
+  //       y: 0,
+  //       x: 1
+  //     }
+  //   },
+  //   {
+  //     id: '3',
+  //     value: 16,
+  //     coord: {
+  //       y: 0,
+  //       x: 2
+  //     }
+  //   },
+  //   {
+  //     id: '4',
+  //     value: 16,
+  //     coord: {
+  //       y: 0,
+  //       x: 3
+  //     }
+  //   }
+  // ]);
 
 
   const onKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
     const direction = getMoveDirection(event);
     if(direction) {
       const newGrid = move(direction, tiles);
-
-      setTiles([...newGrid, generateTile(newGrid, gridSize)])
+      if(JSON.stringify(newGrid) !== JSON.stringify(tiles)) {
+        setTiles([...newGrid, generateTile(newGrid, gridSize)])
+      }
     }
   };
-
-  console.log(tiles)
 
 
   return (
